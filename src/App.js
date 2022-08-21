797ecd7cea9bcb598f9148bfeedddc889942de58
Ibrokhimweb <ibrokhimgithub@gmail.com>
2022-08-21 20:48:07 +0500

    Footer component is complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-// @ts-nocheck
 // Import React
 // import { BrowserRouter } from 'react-router-dom';
+
+// import css
+import './App.css';
 
 // Import Components
 import Navbar from './Components/Navbar/Navbar';
 // import Home from './Components/Home/Home';
+import Carusel from './Components/Carusel/Carusel';
 import About from './Components/About/About';
 import Service from './Components/Service/Service';
+import Footer from './Components/Footer/Footer';
 
-// import css
-import './App.css';
-import Carusel from './Components/Carusel/Carusel';
 
 
 function App() {
     return (
         <>
             <Navbar/>
-            <Carusel/>
             {/* <Home/> */}
+            <Carusel/>
             <About/>
             <Service/>
+            <Footer/>
             {/* <Route path="/" element={<Navbar />} /> */}
         </>
     );
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,80 @@
+// Import React
 import React from 'react';
+
+
+// Import Icons
+import { MdLocationOn, MdLocalPhone, MdMail } from "react-icons/md";
+import { FaTwitter, FaFacebookF, FaLinkedinIn, FaInstagram } from "react-icons/fa";
+
+
+// Import Css
+import './Footer.css'
 
 function Footer(props) {
     return (
         <>
-            <h1>Footer</h1>
+            {/* Footer Section */}
+            <section className="footer">
+                {/* Footer Content */}
+                <div className="footer-content">
+
+                    <div className="footer-get-in">
+                        <h4>GET IN TOUCH</h4>
+                        <div className="footer-get-in__item">
+                            <MdLocationOn />
+                            <p>Parkent, Tashkent ,Uzb</p>
+                        </div>
+                        <div className="footer-get-in__item">
+                            <MdLocalPhone />
+                            <p>+998 99 6057845</p>
+                        </div>
+                        <div className="footer-get-in__item">
+                            <MdMail />
+                            <p>Ibrokhimweb@gmai.com</p>
+                        </div>
+                    </div>
+
+                    <div className="footer-follow-us">
+                        <h4>FOLLOW US</h4>
+                        <p>Amet elitr vero magna sed ipsum sit kasd sea elitr lorem rebum</p>
+                        <dir className="footer-follow-us__icons">
+                            <FaTwitter className="footer-follow-us__icon" />
+                            <FaFacebookF className="footer-follow-us__icon" />
+                            <FaLinkedinIn className="footer-follow-us__icon" />
+                            <FaInstagram className="footer-follow-us__icon" />
+                        </dir>
+
+                    </div>
+
+
+                    <div className="footer-open-hour">
+                        <h4>OPEN HOURS</h4>
+                        <p>MONDAY - FRIDAY</p>
+                        <p className="footer-open-hour__margin" >8.00 AM - 8.00 PM</p>
+                        <p>SATURDAY - SUNDAY</p>
+                        <p>2.00 PM - 8.00 PM</p>
+                    </div>
+
+
+
+                    <div className="footer-newsletter">
+                        <h4>NEWSLETTER</h4>
+                        <p>Amet elitr vero magna sed ipsum sit <br /> kasd sea elitr lorem rebum</p>
+                        <div className="footer__sign-up">
+                            <input type="text" placeholder="Your Email"/>
+                            <button>Sign Up</button>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="footer-after">
+                    <p>Copyright © <a href="#">Domain</a>. All Rights Reserved.</p>
+                    <p>Designed by <a href="#">CYBER STEP</a></p>
+                </div>
+                {/* /Footer Content */}
+            </section>
+            {/* /Footer Section */}
         </>
     );
 }
